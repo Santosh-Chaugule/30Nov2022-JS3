efb289c1c7f2f96c44cff61946a6c9acf86632c7
santosh-chaugule <santoshchaugule1697@gmail.com>
2022-12-01 14:51:25 +0530

    JS

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("I am from the external tag")
+var x = 50;
+var y = 20;
+//variable decleration
+var z;
+
+//variable initialization
+var p;
+p = 40;
+
+
+
+//variable declaration and initialization
+var q = 50;
+
+console.log(x + p);
+
+
